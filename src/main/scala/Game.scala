3365f87712303bd8c702ad8abce6cd8c540328a4
Joana Cardoso <jfgco@iscte-iul.pt>
2026-03-27 17:33:48 +0000

    mudanças no tabuleiro ainda estou a mexer mas em principio ta

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -1,50 +1,55 @@
 import scala.io.StdIn.{readInt, readLine}
 import scala.annotation.tailrec
+import scala.collection.parallel.immutable.ParMap
+
 
 case class Game(
-                   board: List[List[Int]],    
-                   currentPlayer: Int,
+                   board: Board,
+                   currentPlayer: Stone, // ou as pedras pretas ou as brancas
                    message: String = ""
                )
 
 object Game {
-    val empty = 0
-    val white = 1
-    val black = 2
-
-    def printRocks(value: Int): String = value match {
-        case Game.empty => ". "
-        case Game.white => " W "
-        case Game.black => " B "
+    def opponent(s: Stone): Stone = s match {
+        case Stone.Black => Stone.White
+        case Stone.White => Stone.Black
     }
 
-    def printRow(row: List[Int]): Unit = row match {
-        case Nil =>
-        case head :: tail =>
-            print(printRocks(head))
-            printRow(tail)
+    def render(board: Board): Unit = {
+        println("\n   0  1  2  3  4  5  6  7")
+        println("  -------------------------") //para indicar o número na parte de cima do tabuleiro
+        draw(board, 0, 0)
     }
 
-    def renderBoard(board: List[List[Int]], rowNum: Int = 0): Unit = {
-        if (rowNum == 0) println("   0  1  2  3  4  5  6  7\n  -------------------------")
-
-        board match {
-            case Nil => println("  -------------------------")
-            case head :: tail =>
-                print(s"$rowNum |")
-                printRow(head)
+    @tailrec
+    private def draw(board: Board, r: Int, c: Int): Unit = {
+        (r, c) match {
+            case (8, 0) => //caso esteja no final do tabuleiro
+                println("  -------------------------")
+            case (r, 8) => // para todas as linhas com y=8 vai meter um | no final da linha
                 println("|")
-                renderBoard(tail, rowNum + 1)
+                draw(board, r + 1, 0) // desenha o board até aí e recomeça na linha asseguir recursivamente
+            case (r, c) => //caso não se saiba nenhum dos valores
+                if (c == 0) print(s"$r |") // se a coluna for a de índice 0 ou seja inicio da linha ele imprime o número da linha + a |
+                board.get((r, c)) match {
+                    case Some(Stone.Black) => print(" B ")
+                    case Some(Stone.White) => print(" W ")
+                    case None => print(" . ")
+                }
+                draw(board, r, c + 1)
         }
     }
 
     def initBoard(): Game = {
-        val initialBoard = (0 until 8).toList.map { l =>
-            (0 until 8).toList.map { c =>
-                if ((l + c) % 2 == 0) Game.white else Game.black
+        val tabuleiro = (0 until 8).toList.flatMap { r => //flatMap em vez de criar uma lista de listas cria um lista de 64 elementos (8por8)
+            // Para cada linha, criamos 0 a 7 (colunas)
+            (0 until 8).map { c => //aqui crio colunas dentro de cada linha, aqui decidi map pq queremos extamente um par coordenada-> pedra
+                val stone = if ((r + c) % 2 == 0) Stone.White else Stone.Black
+                (r, c) -> stone
             }
-        }
-        Game(initialBoard, Game.black, "O jogo começou! Pretas jogam primeiro.")
+        }.to(scala.collection.parallel.immutable.ParMap)
+
+        Game(tabuleiro, Stone.Black, "Jogo iniciado com flatMap/map!")
     }
 }
 
@@ -53,14 +58,15 @@ object KonaneGame extends App {
     @tailrec
     def gameLoop(estado: Game): Unit = {
         println("\n--- ESTADO DO TABULEIRO ---")
-        Game.renderBoard(estado.board)
+        Game.render(estado.board)
 
         println(s"Mensagem: ${estado.message}")
-        val playerStr = if(estado.currentPlayer == Game.white) "Brancas" else "Pretas"
+        val playerStr = if(estado.currentPlayer == Stone.White) "Brancas" else "Pretas"
         println(s"Vez do Jogador: $playerStr")
 
-        print("\nEscolha a peça (linha): ")
+        print("\nEscolha a peça (linha) ou -1 para sair: ")
         val l1 = readInt()
+
         if (l1 == -1) {
             println("Fim de jogo!")
         } else {
@@ -71,19 +77,19 @@ object KonaneGame extends App {
             print("Destino (coluna): ")
             val c2 = readInt()
 
-            // AQUI: Lógica de processamento da jogada
-            // Por agora, apenas trocamos o turno
-            val proximoJogador = if (estado.currentPlayer == Game.white) Game.black else Game.white
+            // Lógica de Troca de Turno (Usando o Enum Stone)
+            val proximoJogador = Game.opponent(estado.currentPlayer)
 
             val novoEstado = estado.copy(
                 currentPlayer = proximoJogador,
-                message = s"Jogador moveu de ($l1, $c1) para ($l2, $c2)"
+                message = s"O jogador $playerStr tentou mover de ($l1, $c1) para ($l2, $c2)"
             )
 
             gameLoop(novoEstado)
         }
     }
 
+    // Iniciar o loop com o objeto Game completo
     val estadoInicial = Game.initBoard()
     gameLoop(estadoInicial)
 }
---
+++ src/main/scala/RandomMove.scala
@@ -0,0 +1,7 @@
+import scala.util.Random
+object RandomMove {
+    val empty = 0
+    val white = 1
+    val black = 2
+
+}
