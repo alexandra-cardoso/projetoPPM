42a658a64182207a0c50977287557b26dfbb7041
Joana Cardoso <jfgco@iscte-iul.pt>
2026-04-13 17:22:38 +0100

    mudei T3

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,21 +1,45 @@
+object main extends App {
+    println("--- TESTE DE INTEGRAÇÃO (T1, T2, T3 e T4) ---")
 
-    object main extends App {
-        println("--- Teste da Tarefa T1 ---")
+    // 1. Inicializar o tabuleiro e o gerador de números aleatórios
+    val inicialGame = Game.initBoard()
+    val boardWithoutMiddle = inicialGame.board - (3, 3) - (3, 4)
+    val lstOpenCoords = List((3, 3), (3, 4))
+    val rand = MyRandom(123L) // Semente fixa para ser fácil de prever os testes
 
-        // 1. Criar uma semente (seed) aleatória inicial
-        val randomInicial = MyRandom(12345L)
+    println("\n[Teste T4] - Tabuleiro Inicial (Antes da jogada do computador):")
+    Game.render(boardWithoutMiddle)
 
-        // 2. Simular uma lista de posições livres
-        val coordenadasLivres: List[Coord2D] = List((3, 3), (3, 4), (4, 4), (5, 2))
+    // 2. Criar a instância da tua classe T3
+    val computador = new T3()
 
-        println(s"Coordenadas livres disponíveis: $coordenadasLivres")
+    // 3. Fazer a jogada! 
+    // O T3 vai chamar o T1 (RandomMove.randomMove) e o T2 (Logic.play)
+    val jogadaComputador = computador.playRandomly(
+        boardWithoutMiddle,
+        rand,
+        Stone.Black, // Vamos assumir que são as Pretas a jogar
+        lstOpenCoords,
+        RandomMove.randomMove // É aqui que passas o teu T1 como argumento!
+    )
 
-        // 3. Chamar a função randomMove
-        val (jogadaEscolhida, proximoRandom) = RandomMove.randomMove(coordenadasLivres, randomInicial)
+    // O playRandomly devolve uma tupla com 4 valores. Vamos extraí-los:
+    val novoBoardOpt = jogadaComputador._1
+    val novoRandom = jogadaComputador._2
+    val novaListaVazios = jogadaComputador._3
+    val coordenadaDestinoOpt = jogadaComputador._4
 
-        println(s"O computador escolheu a coordenada: $jogadaEscolhida")
+    // 4. Verificar se a jogada funcionou (Teste T2 e T4)
+    novoBoardOpt match {
+        case Some(board) =>
+            println(s"\n[Teste T1 e T3] - Sucesso! O computador saltou para a coordenada: ${coordenadaDestinoOpt.get}")
+            println(s"[Teste T2] - A nova lista de espaços vazios é: $novaListaVazios")
 
-        // 4. Testar uma segunda vez com o novo estado do Random
-        val (jogadaEscolhida2, proximoRandom2) = RandomMove.randomMove(coordenadasLivres, proximoRandom)
-        println(s"Na segunda tentativa, com a nova seed, escolheu: $jogadaEscolhida2")
-    } 
+            println("\n[Teste T4] - Tabuleiro Após a Jogada:")
+            Game.render(board)
+
+        case None =>
+            println("\n[Teste T2 e T3] - O computador não conseguiu encontrar uma jogada válida.")
+            println("Destino tentado foi: " + coordenadaDestinoOpt)
+    }
+}
