e14e9d0eb064375fa415e44a6ac182dfa0223b0c
Joana Cardoso <jfgco@iscte-iul.pt>
2026-04-13 17:01:54 +0100

    T1
    fiz o T1, T3 ta com erro

--- src/main/scala/RandomMove.scala
+++ src/main/scala/RandomMove.scala
@@ -1,7 +1,22 @@
 import scala.util.Random
 object RandomMove {
-    val empty = 0
-    val white = 1
-    val black = 2
+    
+    def randomMove(lstOpenCoords: List[Coord2D], rand: MyRandom): (Coord2D, MyRandom) = {
+        // Garanto que a lista tem elementos 
+        require(lstOpenCoords.nonEmpty, "A lista de coordenadas livres não pode estar vazia")
+
+        // 2. Gerar um índice aleatório entre 0 e o tamanho da lista - 1
+        val (index, nextRand) = rand.nextInt(lstOpenCoords.length)
+
+       
+        // para garantir que devolvemos um MyRandom
+        val nextMyRandom = nextRand match {
+            case mr: MyRandom => mr
+            case _ => MyRandom(0L) 
+        }
+        val chosenCoord = lstOpenCoords(index)// a coordenada escolhida vai ser a no index escolhido aleatóriamente
+        
+        (chosenCoord, nextMyRandom)
+    }
 
 }
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,3 +1,21 @@
-object main {
-    
-}
+
+    object main extends App {
+        println("--- Teste da Tarefa T1 ---")
+
+        // 1. Criar uma semente (seed) aleatória inicial
+        val randomInicial = MyRandom(12345L)
+
+        // 2. Simular uma lista de posições livres
+        val coordenadasLivres: List[Coord2D] = List((3, 3), (3, 4), (4, 4), (5, 2))
+
+        println(s"Coordenadas livres disponíveis: $coordenadasLivres")
+
+        // 3. Chamar a função randomMove
+        val (jogadaEscolhida, proximoRandom) = RandomMove.randomMove(coordenadasLivres, randomInicial)
+
+        println(s"O computador escolheu a coordenada: $jogadaEscolhida")
+
+        // 4. Testar uma segunda vez com o novo estado do Random
+        val (jogadaEscolhida2, proximoRandom2) = RandomMove.randomMove(coordenadasLivres, proximoRandom)
+        println(s"Na segunda tentativa, com a nova seed, escolheu: $jogadaEscolhida2")
+    } 
