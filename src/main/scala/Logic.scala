c90af92223f473754d224dcde983ec2fa996d7da
Rui-repo <ruifernandesiscte@gmail.com>
2026-05-07 10:28:31 +0100

    notas entrega intercalar

--- src/main/scala/Logic.scala
+++ src/main/scala/Logic.scala
@@ -1,4 +1,10 @@
 import scala.annotation.tailrec
+// Notas de primeira entrega: 
+// Criar teste na interface para o random move
+// Mais comentários
+// FindCoordFrom devia retornar a lista para poder escolher a jogada de forma random ou com alguma verificação
+// por exemplo escolher a peçá que podia capturar mais do que uma vez.
+
 
 object Logic {
     def play(board: Board, player: Stone, coordFrom: Coord2D, coordTo: Coord2D, lstOpenCoords: List[Coord2D]):(Option[Board], List[Coord2D])= {
