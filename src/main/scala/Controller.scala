081511cee370dfcc70824b85932773574847b6d8
Joana Cardoso <jfgco@iscte-iul.pt>
2026-05-18 17:05:26 +0100

    adicionei coments

--- src/main/scala/Controller.scala
+++ src/main/scala/Controller.scala
@@ -8,18 +8,18 @@ import javafx.scene.input.MouseEvent
 
 class Controller {
 
-    @FXML var boardGrid: GridPane = _
+    @FXML var boardGrid: GridPane = _ //usamos um grippane do javafx para desenhar o tabuleiro
     @FXML var lblStatus: Label = _ //controla as mensagens que vamos mandando aos jogadores ao longo do jogo
-    @FXML var btnRestart: Button = _
-    @FXML var btnRandom: Button = _
-    @FXML var btnUndo: Button = _
+    @FXML var btnRestart: Button = _ //botão de recomeçar
+    @FXML var btnRandom: Button = _//botão de jogada random
+    @FXML var btnUndo: Button = _//botão de desfazer uma jogada
     @FXML var lblTimer: Label = _ //controla o tempo de cada jogada
 
     var showHints: Boolean = true // Fácil e Médio mostram jogadas para onde o jogador pode ir
     var timeLimit: Int = 90 // Fácil = 90s, Médio/Difícil = 30s
     var timer: javafx.animation.Timeline = _ //faz a animação do relógio e do tempo a passar
     var tempoRestante: Int = 90
-    var cells: Map[Coord2D, Pane] = Map()
+    var cells: Map[Coord2D, Pane] = Map() //utilizamos  um mapa para guardar as coordenadas de cada peça e o painel em que estão
     var history: List[(Game, List[Coord2D])] = List() //guarda o estado da jogada para ser possível fazer o undo
     //aqui em baixo meti as vaiáveis necessárias para o board, como o board na GUI tem uma dimensão fixa, o valor de rows e de cols são "val"
     val ROWS = 6
@@ -27,7 +27,7 @@ class Controller {
     var currentBoard: Board = _
     var currentOpenCoords: List[Coord2D] = List() //a lista de coordenadas vazia está incialmente vazia
     var currentPlayer: Stone = Stone.Black //começam as peças pretas
-    var selectedCoord: Option[Coord2D] = None //ao ínicio n\ao existe nenhuma peça selecionada
+    var selectedCoord: Option[Coord2D] = None //ao ínicio não existe nenhuma peça selecionada
     var currentRand: MyRandom = MyRandom(System.currentTimeMillis()) //variável para as jogadas random
 
     @FXML
@@ -53,13 +53,13 @@ class Controller {
         iniciarNovoJogo()// Arranca o jogo
     }
     def iniciarNovoJogo(): Unit = { //metodo que inicia o tabuleiro e arrnaca o jogo
-        history = List()
+        history = List() //começa vazia quando inicio o jogo pq ainda n foram feitas nenhumas jogadas
         val inicialGame = Game.initBoard(ROWS, COLS) // Usamos a função Game.initBoard feita na primeira parte do trabalho PROVAVALEMENTE VAI MUDAR PARA O FICHIERO LOGIC COMO DICA DO PROF
         // Para permitir a escolha de tirar das pontas ou centro, o tabuleiro começa CHEIO
         currentBoard = inicialGame.board
         currentOpenCoords = List() // Começa vazia para entrar na fase de remoção
-        currentPlayer = Stone.Black
-        selectedCoord = None
+        currentPlayer = Stone.Black //começam as peças pretas.
+        selectedCoord = None//começa sem qualquer coordenada selecionada
         renderBoard() //chama a função que "desenha" o visual do tabuleiro
         iniciarTimer() //inicia o timer de cada jogada, dependo do nível de dificuldade escolhido
         lblStatus.setText("Jogo iniciado! Pretas: tirem uma peça do Centro ou Canto.")
@@ -124,9 +124,9 @@ class Controller {
                                 source.setStyle("-fx-background-color: rgba(0, 255, 0, 0.4); -fx-border-color: #cccccc;") //colocamos o fundo a verde
                                 lblStatus.setText(s"Peça $clickedCoord selecionada. Escolhe o destino.") //coloca esta mensagem no painel de jogo
 
-                                if (showHints) {
-                                    val destinos = obterDestinosValidos(clickedCoord, currentPlayer, currentBoard)
-                                    destinos.foreach(d => destacarCelula(d, "rgba(255, 0, 0, 0.4)"))
+                                if (showHints) { //se tiver num nível de dificuldade em que showHints está a true, significa que pode destacar os caminhos para onde se pode deslocar a peça selecionada
+                                    val destinos = obterDestinosValidos(clickedCoord, currentPlayer, currentBoard) //obtem os destinos validos para a peça selecionada
+                                    destinos.foreach(d => destacarCelula(d, "rgba(255, 0, 0, 0.4)")) //mete a vermelho os sítios para onde pode ir
                                 }
 
                             case Some(_) => //caso a peça selecionada para jogar seja do adversário, enviamos uma mensagem a avisar o jogador
@@ -137,41 +137,41 @@ class Controller {
                         }
 
                     case Some(fromCoord) => //caso esteja a carregar numa coordenada de origem que já estava selecionada
-                        (clickedCoord == fromCoord) match { // <-- if/else substituído por match
+                        (clickedCoord == fromCoord) match { //verifica se a peça selecionada é a peça de origem
                             case true => //caso a coordenada carregada corresponda à que já foi definida como origem
                                 selectedCoord = None
                                 renderBoard() //desseleciono a peça
 
                             case false => // se carreguei noutra coordenada, posso fazer a jogada
                                 val (optBoard, newOpenCoords) = Logic.play(
-                                    currentBoard, currentPlayer, fromCoord, clickedCoord, currentOpenCoords //chamo a lógica do play feita na primeira parte do trabalho
+                                    currentBoard, currentPlayer, fromCoord, clickedCoord, currentOpenCoords //chamo a lógica do play feita na primeira parte do trabalho no ficheiro Logic
                                 )
                                 optBoard match {
                                     case Some(newBoard) => //caso a jogada tenha sido válida
-                                        history = (Game(currentBoard, ROWS, COLS, currentPlayer, "Undo realizado"), currentOpenCoords) :: history
+                                        history = (Game(currentBoard, ROWS, COLS, currentPlayer, "Undo realizado"), currentOpenCoords) :: history //guardamos na lista para podermos fazer o Undo eventualemnete
                                         currentBoard = newBoard //criamos um novo tabuleiro, para retirar as peças da posição em que estavam
                                         currentOpenCoords = newOpenCoords //atualiza a lista de posições vazias
 
                                         // Verifica se pode saltar novamente com a MESMA peça
-                                        Logic.podeSaltarMais(newBoard, clickedCoord, currentPlayer, ROWS, COLS) match { // <-- if/else substituído por match
+                                        Logic.podeSaltarMais(newBoard, clickedCoord, currentPlayer, ROWS, COLS) match { // chama a lógica para ver se pode saltar mais que uma vez com a mesma peça do ficheiro Logic
                                             case true =>
                                                 selectedCoord = Some(clickedCoord) // Mantém a peça selecionada no novo lugar
-                                                renderBoard()
-                                                destacarCelula(clickedCoord, "rgba(255, 255, 0, 0.4)") // coloquei a amarelo para indicar que pode continuar
+                                                renderBoard()//desenha o tabuleiro atualizado
+                                                destacarCelula(clickedCoord, "rgba(255, 255, 0, 0.4)") // destaca a amarelo para indicar que pode continuar, ao contrário de quando pode jogar apenas uma vez e aparece a verde
 
-                                                val proximosDestinos = obterDestinosValidos(clickedCoord, currentPlayer, newBoard)
+                                                val proximosDestinos = obterDestinosValidos(clickedCoord, currentPlayer, newBoard) //vejo todos os destinos possíveis para ainda posso andar e meto destacado
                                                 proximosDestinos.foreach(d => destacarCelula(d, "rgba(255, 0, 0, 0.4)"))
 
                                                 lblStatus.setText("Captura múltipla! Continua ou clica na peça para terminar.")
 
-                                            case false =>
+                                            case false => //se n posso saltar mais então acaba o turno daquela peça onde se verifica se ela é a peça vencedora ou não
                                                 finalizarTurno()
                                         }
 
                                     case None => //caso o play não ocorra com sucesso (logo não devolve nenhum tabuleiro)
                                         lblStatus.setText("Salto inválido! Escolhe novamente.") //enviamos uma mensagem de erro para o jogador
                                         // Se não houver salto múltiplo disponível, limpamos a seleção
-                                        Logic.podeSaltarMais(currentBoard, fromCoord, currentPlayer, ROWS, COLS) match { // <-- if substituído por match
+                                        Logic.podeSaltarMais(currentBoard, fromCoord, currentPlayer, ROWS, COLS) match {
                                             case false =>
                                                 selectedCoord = None
                                                 renderBoard()
@@ -183,61 +183,64 @@ class Controller {
         }
     }
     def finalizarTurno(): Unit = {
-        Logic.verificarVencedor(currentBoard, ROWS, COLS, currentPlayer, currentOpenCoords) match {
-            case Some(vencedor) =>
-                if (timer != null) timer.stop()
-                lblTimer.setText("")
+        Logic.verificarVencedor(currentBoard, ROWS, COLS, currentPlayer, currentOpenCoords) match { //usa a logica criada em Logic para verificar se existe um vencedor.
+            case Some(vencedor) => //se existe um vencedor
+                if (timer != null) timer.stop()//o timer ainda está a correr mas podemos pará-lo
+                lblTimer.setText("")//deixa de mostrar texto o timer
+                val nomeVencedor = if (vencedor == Stone.Black) "Pretas" else "Brancas" // identifica o vencedor
+                lblStatus.setText(s"$nomeVencedor venceram o jogo!") // linha que faltava que mostra quem ganhou no ecrã
                 return
-            case None => ()
+            case None => ()//caso n exista vencedor n faz nada
         }
 
-        currentPlayer = Game.opponent(currentPlayer)
-        selectedCoord = None
-        renderBoard()
+        currentPlayer = Game.opponent(currentPlayer)//muda para o oponente
+        selectedCoord = None //deseleciona a peça
+        renderBoard()//mostra o tabuleiro atualizado
         val nomeJogador = if (currentPlayer == Stone.Black) "Pretas" else "Brancas"
-        lblStatus.setText(s"Vez das $nomeJogador.")
-        iniciarTimer() // <-- acrescenta esta linha
+        lblStatus.setText(s"Vez das $nomeJogador.") //informa quem ºe a jogar
+        iniciarTimer() //inicia um novo timer sempre que um jogador termina a sua jogada
     }
 
-    def undoJogada(): Unit = {
-        Logic.undo(history) match {
-            case None =>
+    def undoJogada(): Unit = { //metodo que se encarrega de desfazer a jogada quando carregamos no botaõ Undo
+        Logic.undo(history) match { //chamamos o metodo undo fieto no fichiero Logic que recebe a lista history que temos estado a ir guardando aso longo do jogo
+            case None => //caso não exista nenhuma lista ou esteja vazia
                 lblStatus.setText("Não há mais jogadas para desfazer!")
             case Some(((oldGame, oldOpenCoords), remainingHistory)) =>
                 // Atualiza as variáveis do Controller com os dados do objeto Game recuperado
                 currentBoard = oldGame.board
                 currentPlayer = oldGame.currentPlayer
-                // Nota: ROWS e COLS são 'val' no  controller, por isso não mudam,
+                // como ROWS e COLS são 'val' no  controller, não mudam
                 //Atualiza a lista de buracos e o histórico
-                currentOpenCoords = oldOpenCoords
-                history = remainingHistory
+                currentOpenCoords = oldOpenCoords //atualiza alista de opencorrds para ser a antiga
+                history = remainingHistory // a lista passa a ser tbm a anterior
                 //Limpa seleções visuais e redesenha o tabuleiro
                 selectedCoord = None
-                renderBoard()
-                // Atualiza a mensagem na UI
+                renderBoard() //mostra o tabuleiro atualizado
+                // Atualiza a mensagem que aparece
                 val nome = if (currentPlayer == Stone.Black) "Pretas" else "Brancas"
                 lblStatus.setText(s"Desfeito! Vez das $nome.")
-                iniciarTimer()
+                iniciarTimer()//inicia novamente um timer
         }
     }
     def removerPecaInicial(coord: Coord2D): Unit = { //metodo que remove visualmente a peça do tabuleiro
-        history = (Game(currentBoard, ROWS, COLS, currentPlayer, "Undo realizado"), currentOpenCoords) :: history
-        currentBoard = currentBoard - coord
+        history = (Game(currentBoard, ROWS, COLS, currentPlayer, "Undo realizado"), currentOpenCoords) :: history // a peça retirada incialmente também é uma jogada que pode ser desfeita
+        currentBoard = currentBoard - coord //tiro essa coordenada co tabuleiro
         currentOpenCoords = coord :: currentOpenCoords //adiciono essa coordernada à lista de coordenadas livres
         currentPlayer = Game.opponent(currentPlayer)//troco de jogador
-        renderBoard()
+        renderBoard() //mostra tabuleiro atualizado
     }
 
     def obterDestinosValidos(pos: Coord2D, p: Stone, board: Board): List[Coord2D] = {//metodo que verifica os destinos validos para poder assinala-los a vermelho lá em cima
         val directions = List((2, 0), (-2, 0), (0, 2), (0, -2)) //crio uma lista de direções com as 4 direções que me posso mover
-        directions.flatMap { case (dr, dc) =>
-            val target = (pos._1 + dr, pos._2 + dc)
-            val mid = (pos._1 + dr / 2, pos._2 + dc / 2)
+        directions.flatMap {
+            case (dr, dc) =>
+            val target = (pos._1 + dr, pos._2 + dc) // crio a target como a coordenada onde tamos mais a direção selecionada
+            val mid = (pos._1 + dr / 2, pos._2 + dc / 2) // e a do meio vai ser a atual masi metade da direção selecionada
 
-            val dentro = target._1 >= 0 && target._1 < ROWS && target._2 >= 0 && target._2 < COLS
-            (dentro, board.contains(target), board.get(mid)) match {
-                case (true, false, Some(mid_stone)) if mid_stone != p => Some(target)
-                case _ => None
+            val dentro = target._1 >= 0 && target._1 < ROWS && target._2 >= 0 && target._2 < COLS // se estiver dentro do tabuleiro vai sempre estar tanto as linhas como as colunas entre 0 e 6
+            (dentro, board.contains(target), board.get(mid)) match { // se estou dentro, o board tem a posição target e a posição do meio é uma peça oponente
+                case (true, false, Some(mid_stone)) if mid_stone != p => Some(target) //então devolvo a posição de destino selecionada como válida
+                case _ => None //se n respeitar a condição n devolvo nenhuma coordenada pq n existe nenhuma válida
             }
         }
     }
